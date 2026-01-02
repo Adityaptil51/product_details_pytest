51f5bd5bf263f4bc628ca08c27781181b730e6d9
Adityaptil51 <adityaapatil51@gmail.com>
2026-01-02 22:18:04 +0530

    Create product_details.py

---
+++ product_details.py
@@ -0,0 +1,11 @@
+def product_details(prod_id, name, quantity, price):
+    result = (
+        f"Product ID: {prod_id}\n"
+        f"Product Name: {name}\n"
+        f"Quantity: {quantity}\n"
+        f"Price: ${price}"
+    )
+    return result
+
+if __name__ == "__main__":
+    print(product_details(101, "iPhone", 1, 155000))
